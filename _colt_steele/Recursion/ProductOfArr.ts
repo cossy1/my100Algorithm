208c9c0aa68e9b7916227e9b7dc15d134099cd9a
Adegoke <unique>
2021-05-14 00:29:27 +0100

    product of aray revamp

--- _colt_steele/Recursion/ProductOfArr.ts
+++ _colt_steele/Recursion/ProductOfArr.ts
@@ -10,3 +10,10 @@ function ProductOfArr (arr: number[]) {
 };
 
 console.log(ProductOfArr([1, 2, 3, 4, 5]));
+
+function ProductOfArr2 (arr:number[]) {
+  if(arr.length === 0) return 1;
+  return arr[0] * ProductOfArr2(arr.slice(1));
+}
+
+console.log(ProductOfArr2([1, 2, 3, 4, 5]));
