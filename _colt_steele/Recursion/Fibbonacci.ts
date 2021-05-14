505f0f859844bc5d6cb60580c5ead3df523c7e72
Adegoke <unique>
2021-05-14 22:36:33 +0100

    fibonacci sequence

--- _colt_steele/Recursion/Fibbonacci.ts
+++ _colt_steele/Recursion/Fibbonacci.ts
@@ -1,3 +1,9 @@
-function Fibonacci (num: number) {
 
-}
+function Fibonacci (n: number) {
+  if (n <= 2) return 1;
+  return Fibonacci(n - 1 ) + Fibonacci( n - 2 );
+};
+
+console.log(Fibonacci(4));
+console.log(Fibonacci(10));
+console.log(Fibonacci(28));
