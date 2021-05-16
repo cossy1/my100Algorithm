cb12fab558d70878f99f2fb19f468ba3efa1ae64
Adegoke <unique>
2021-05-16 14:25:12 +0100

    capitalize ist letter

--- _colt_steele/Recursion/CapitalizeFirst.ts
+++ _colt_steele/Recursion/CapitalizeFirst.ts
@@ -1,15 +1,16 @@
-function Capitalize(arr: string[]) {
+function CapitalizeIst(arr: string[]) {
   let newArr = [];
   let str = '';
 
   if (arr.length === 0) return newArr;
-  for ( var i = 0; i < arr[0].length - 1; i++ ) {
+  if (arr[0].length === 1) str = arr[0].toUpperCase();
+  for (var i = 0; i < arr[0].length - 1; i++) {
      if (i === 0) str = arr[0][i].toUpperCase();
      str += arr[0][i + 1];
   }
   newArr.push(str);
-  return newArr.concat(Capitalize(arr.slice(1)));
+  return newArr.concat(CapitalizeIst(arr.slice(1)));
 
 }
 
-console.log(Capitalize(['car','taco','banana']));
+console.log(CapitalizeIst(['', 'c', 'Der','taco','d', 'banana', 'a', '']));
