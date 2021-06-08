253a8060a0198e35599825d24bf1ce546d3d6b0f
Adegoke <adeg4ife@gmail.com>
2021-06-08 10:35:22 +0100

    selection sort completed

--- _colt_steele/sorting/index.ts
+++ _colt_steele/sorting/index.ts
@@ -1,8 +1,12 @@
-//Bubble sort (n2)
+
+//swap
+const swap = (arr, idx1, idx2) => [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+
+/** Bubble sort (n2) **/
 function bubbleSort <T>(arr: Array<T>) {
   let noSwap;
-  const swap = (arr, idx1, idx2) => [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
   for (let i = arr.length; i > 0; i--) {
+    console.log(arr[i]);
     noSwap = true;
     for (let j = 0; j < i - 1; j++) {
       if ( arr[j] > arr[j + 1]) {
@@ -17,13 +21,16 @@ function bubbleSort <T>(arr: Array<T>) {
 
 console.log(bubbleSort([12, 11, 13, 17, 19, 6]));
 
-//selection sort
+/** selection sort () **/
 function selectionSort <T>(arr: Array<T>) {
   for (let i = 0; i < arr.length; i++) {
     let lowestIndex = i;
-    for (let j = i - 1; j < arr.length; j++) {
-
+    for (let j = i + 1; j < arr.length; j++) {
+      if(arr[lowestIndex] > arr[j]) {
+        lowestIndex = j
+      }
     }
+    if (i !== lowestIndex) swap(arr, i, lowestIndex);
   }
   return arr
 }
