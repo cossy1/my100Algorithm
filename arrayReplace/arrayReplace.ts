c7f2c05b7284f60c1681c8ca0281217841158cd0
Michaelceke <ekemicheal@yahoo.com>
2021-09-14 15:57:50 +0100

    solved

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,11 @@
 function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-   
+   const newArray=inputArray.map(item=>{
+       if (item===elemToReplace) {
+           return substitutionElem
+       }
+       else return item
+   });
+    return newArray;
 }
 
 console.log(arrayReplace([1, 2, 1], 1, 3));
