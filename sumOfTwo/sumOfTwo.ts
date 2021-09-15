ca582157c69993b71c9ad9a2a97cd32b8579f6f0
cossy1 <onyekwuocosmas@gmail.com>
2021-09-15 12:22:17 +0100

    sum of two

--- sumOfTwo/sumOfTwo.ts
+++ sumOfTwo/sumOfTwo.ts
@@ -1,4 +1,15 @@
-function sumOfTwo(a: number[], b: number[], v: number): boolean {
+function sumOfTwo(a: number[], b: number[], v: number) {
+    let res: boolean = false;
+
+    for(let i=0;i<a.length; i++){
+        for(let j=0; j<b.length; j++){
+            if(a[i] + b[j] === v)
+                res = true;
+        }
+
+    }
+
+    return res;
 
 }
 
