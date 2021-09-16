80efbd50b087063160fc5aa7ffb46f80e1eaeac2
cossy1 <onyekwuocosmas@gmail.com>
2021-09-16 07:39:57 +0100

    sum odd fibonacci nums

--- sumOddFibonacciNums/sumOddFibonacciNums.ts
+++ sumOddFibonacciNums/sumOddFibonacciNums.ts
@@ -1,4 +1,16 @@
 function sumOddFibonacciNums(num: number) {
+    let resArr:number[] = [1, 1];
+
+    for(let i=0, j=i+1; i<=resArr.length, j<resArr.length; i++, j++){
+        let val = resArr[i] + resArr[j];
+        if(val < num)
+            resArr.push(val)
+    }
+
+    let ans = resArr.filter((element) => element%2 !== 0).reduce((a, b) => a + b);
+
+
+    return ans;
 
 }
 
