20588356a282ad3bfe7038ce82527f2eee96b6ea
Michaelceke <ekemicheal@yahoo.com>
2021-09-19 00:44:40 +0100

    solved

--- largestNumber/largestNumber.ts
+++ largestNumber/largestNumber.ts
@@ -1,5 +1,9 @@
 function largestNumber(n: number): number {
- 
+    let numStr="";
+    for (let i=0; i<n; i++){
+        numStr=numStr+"9"
+    }
+    return parseInt(numStr)
 }
 
 console.log(largestNumber(2));
