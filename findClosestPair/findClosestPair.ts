935b72c23cdd9f4b570f235995cc4fc9da9618e7
Damian <emmydollar98@gmail.com>
2021-09-24 12:36:29 +0100

    implemented findClosestPair

--- findClosestPair/findClosestPair.ts
+++ findClosestPair/findClosestPair.ts
@@ -1,6 +1,19 @@
-function findClosestPair(numbers: number[], sum: number): number {
-   
-}
+function findClosestPair(number: number[], sum: number): number {
+  let sumArray: number[] = [];
+  let arrL: number = number.length;
 
-console.log(findClosestPair([1, 0, 2, 4, 3, 0], 5));
+  for (let i = 0; i < arrL; i++) {
+    for (let j = arrL - 1; j >= i + 1; j--) {
+      number[i] + number[j] === sum &&
+        sumArray.push(
+          Math.abs(number.indexOf(number[i]) - number.indexOf(number[j]))
+        );
+    }
+  }
+  return sumArray.length !== 0 ? sumArray.sort()[0] : -1;
+}
+let numbers = [1, 0, 2, 4, 3, 0],
+  sum = 5;
+
+console.log(findClosestPair(numbers, sum));
 console.log(findClosestPair([2, 3, 7], 8));
