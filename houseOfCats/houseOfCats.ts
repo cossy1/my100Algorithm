10e6d5625d7a0b32ce28c950ac62808bb25d9103
Damian <emmydollar98@gmail.com>
2021-10-02 21:15:39 +0100

    implemented houseOfCats

--- houseOfCats/houseOfCats.ts
+++ houseOfCats/houseOfCats.ts
@@ -1,6 +1,9 @@
 function houseOfCats(legs: number): number[] {
-
+  let result: number[] = [];
+  while (legs % 2 === 0) {
+    result.unshift(legs / 2);
+    legs -= 4;
+  }
+  return result;
 }
-
 console.log(houseOfCats(6));
-console.log(houseOfCats(2));
