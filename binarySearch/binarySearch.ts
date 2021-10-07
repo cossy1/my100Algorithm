13ff9e49d47ac207c76b25408b2532e19367f2df
cossy1 <onyekwuocosmas@gmail.com>
2021-10-07 07:44:01 +0100

    binary search

--- binarySearch/binarySearch.ts
+++ binarySearch/binarySearch.ts
@@ -4,6 +4,7 @@ const ar = [1, 0, 3, 2, 4, 5, 8, 9, 7, 6];
 const searchNum: any = (arr: number[], num: number, start: number, end: number) => {
     let sortedArray = arr.sort((a, b) => a - b);
     if(start > end) return num + ' does not exist in the array';
+    // get the midpoint of the array
     let midPoint = Math.floor((start + end)/2);
     if(sortedArray[midPoint] === num) return num + ' found at position ' + midPoint;
     if(sortedArray[midPoint] > num) return searchNum(arr, num, start, midPoint - 1);
