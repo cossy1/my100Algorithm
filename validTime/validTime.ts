c51d27e57eff4498a0433490777b91a90e4082a0
cossy1 <onyekwuocosmas@gmail.com>
2021-10-07 08:01:06 +0100

    chore: valid time

--- validTime/validTime.ts
+++ validTime/validTime.ts
@@ -1,4 +1,11 @@
-function validTime(time: string): boolean {
+function validTime(time: string) {
+    let res: boolean = false;
+    let newTime = time.split(":");
+
+    if((parseInt(newTime[0]) >= 0 && parseInt(newTime[0]) <=23) && parseInt(newTime[1]) >= 0 && parseInt(newTime[1]) <=59)
+        res = true;
+
+    return res;
 
 }
 
